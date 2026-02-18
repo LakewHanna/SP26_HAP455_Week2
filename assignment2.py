72d17d0d4b9633dd7b5b364660e7e1cf31be94ad
Hanna Lakew <hlakew@gmu.edu>
2026-02-18 17:50:02 -0500

    Add health function for lak_han

--- assignment2.py
+++ assignment2.py
@@ -19,6 +19,7 @@ def main():
     rob_gab()
     jia_xun()
     nav_ros()
+    lak_han()
     # ==== END STUDENT FUNCTION CALLS ====
 
 
@@ -87,6 +88,11 @@ def nav_ros():
     print("Patient Name: Lisa Thompson")
     print("Patient Age: 52")
     print("Diagnosis: Heart Disease")
+
+def lak_han () :
+    print("Patient Name: Helen Hunt")
+    print("Patient Age: 77")
+    print("Diagnosis: Asthma")
 
 # ==== END STUDENT CODE SECTION ====
 
